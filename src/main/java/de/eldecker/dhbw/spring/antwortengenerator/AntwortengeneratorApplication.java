34de70bfb81ce188fdc3e28d0ce2b00b50e1577b
MDecker-MobileComputing <mide_42b@yahoo.de>
2026-06-14 12:33:57 +0200

    Projekt startet jetzt und beenden sich gleich wieder

---
+++ src/main/java/de/eldecker/dhbw/spring/antwortengenerator/AntwortengeneratorApplication.java
@@ -0,0 +1,15 @@
+package de.eldecker.dhbw.spring.antwortengenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class AntwortengeneratorApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run( AntwortengeneratorApplication.class, args );
+	}
+
+}
---
+++ src/test/java/de/eldecker/dhbw/spring/antwortengenerator/AntwortengeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package de.eldecker.dhbw.spring.antwortengenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntwortengeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
